3c012e886306f265de63b6777c0063933b6a001a
XFGQ <furkanduman1925@gmail.com>
2025-12-25 16:53:39 +0300

    Add initial project setup with Playwright configuration and login page implementation

---
+++ pages/LoginPage.ts
@@ -0,0 +1,32 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    // Elements
+    this.emailInput = page.locator('input[type="email"]');
+    this.passwordInput = page.locator('input[type="password"]');
+    this.loginButton = page.locator('button[type="submit"]');
+
+
+
+    //for error message
+     this.errorMessage = page.locator('.alert-danger, .error-message, text=invalid'); 
+  }
+
+  async goto() {
+    await this.page.goto('https://sweetshop.netlify.app/login');
+  }
+
+  async login(email: string, pass: string) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(pass);
+    await this.loginButton.click();
+  }
+}
